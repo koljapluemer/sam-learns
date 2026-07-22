d18a45da13925c0e8f338881030783aed9d34732
Kolja Sam <github@koljapluemer.com>
2026-07-22 10:12:50 +0200

    add country groups, reformat meta, add gc

--- src/shared/info/InfoPanel.vue
+++ src/shared/info/InfoPanel.vue
@@ -6,77 +6,40 @@ const emit = defineEmits<{ close: [] }>()
 </script>
 
 <template>
-  <dialog
-    class="modal"
-    :class="{ 'modal-open': isOpen }"
-  >
-    <div class="modal-box">
-      <h3 class="text-lg font-bold">
-        About
-      </h3>
+  <dialog class="modal" :class="{ 'modal-open': isOpen }">
+    <div class="modal-box flex flex-col gap-2">
 
-      <div
-        v-if="shellState.about"
-        class="mt-4"
-      >
+      <div v-if="shellState.about">
         <component :is="shellState.about" />
       </div>
 
-      <div class="mt-4 flex flex-col items-center gap-4 text-center text-sm text-base-content/70">
-        <p class="flex flex-row items-center gap-4">
-          <span>
-            Made with ♥ by <a
-              href="https://koljapluemer.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              class="link"
-            >Kolja Sam</a>
-          </span>
-          <a
-            href="https://ko-fi.com/S6S81CWUVD"
-            target="_blank"
-          ><img
-            height="36"
-            style="border:0px;height:36px;"
-            src="https://storage.ko-fi.com/cdn/kofi4.png?v=6"
-            border="0"
-            alt="Buy Me a Coffee at ko-fi.com"
-          ></a>
-        </p>
+      <p>
+        Made with ♥ by <a href="https://koljasam.com" target="_blank" rel="noopener noreferrer" class="link">Kolja
+          Sam</a>.
+      </p>
+      <p>
+        If you want to support me building more apps like this in the future,
+        <a href="https://ko-fi.com/S6S81CWUVD" target="_blank" rel="noopener" class="link">
+          support my work on ko-fi
+        </a>.
+      </p>
 
-        <p>
-          I'm using the privacy-friendly <a
-            href="https://www.goatcounter.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="link"
-          >Goatcounter</a> to track page views and I store some pseudonymous learning data. No
-          personal data is collected, and cookies are used solely for tracking your learning progress on your
-          device. This app is
-          <a
-            href="https://github.com/koljapluemer/sam-learns-things"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="link"
-          >open source</a>.
-        </p>
-      </div>
-
-      <div class="modal-action">
-        <button
-          type="button"
-          class="btn btn-sm"
-          @click="emit('close')"
-        >
-          Close
-        </button>
-      </div>
+      <p>
+        I'm using the privacy-friendly <a href="https://www.goatcounter.com" target="_blank" rel="noopener noreferrer"
+          class="link">Goatcounter</a> to track page views and I store some pseudonymous learning data. No
+        personal data is collected, and cookies are used solely for tracking your learning progress on your
+        device. This app is
+        <a href="https://github.com/koljapluemer/sam-learns-things" target="_blank" rel="noopener noreferrer"
+          class="link">open source</a>.
+      </p>
     </div>
-    <form
-      method="dialog"
-      class="modal-backdrop"
-      @click="emit('close')"
-    >
+
+    <div class="modal-action">
+      <button type="button" class="btn btn-sm" @click="emit('close')">
+        Close
+      </button>
+    </div>
+    <form method="dialog" class="modal-backdrop" @click="emit('close')">
       <button>close</button>
     </form>
   </dialog>
